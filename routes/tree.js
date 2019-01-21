16dcfe86ba5dbecd5763b4a907c910caf930c7b6
Unknown <michael.ochsner@hotmail.com>
2019-01-21 23:29:09 +0100

    rune dropdowns

--- routes/tree.js
+++ routes/tree.js
@@ -11,6 +11,7 @@ module.exports.controller = function(app) {
 		opt.treeTitle = "Skill Tree",
 		opt.treeDesc = "allocate points and check your power",
 		opt.points = 0
+		opt.bRunes = bRunes
 
 		BC.getTree(res, 'tree', opt)
 
@@ -18,4 +19,79 @@ module.exports.controller = function(app) {
 
 
 	});
+}
+
+var bRunes = {
+	perf : {
+		name: "Perfectionist", 
+		color: "red"
+	},
+	consi : {
+		name: "Consistent Onslaugh", 
+		color: "red"
+	},
+	mana : {
+		name: "Mana Hunger", 
+		color: "red"
+	},
+	narr : {
+		name: "Narrow Sighted Magic", 
+		color: "red"
+	},
+	aggr : {
+		name: "Aggressive Aggitator", 
+		color: "red"
+	},
+	adre : {
+		name: "Adrenaline Rush", 
+		color: "red"
+	},
+	regen: {
+		name: "Regeneration of the Brute",
+		color: "green"
+	},
+	bid: {
+		name: "Biding Time",
+		color: "green"
+	},
+	arca: {
+		name: "Arcane Surge",
+		color: "green"
+	},
+	blood: {
+		name: "Blood Pact",
+		color: "green"
+	},
+	bulw: {
+		name: "Bulwark of the Surgeon",
+		color: "green"
+	},
+	bliss: {
+		name: "Blissful Ignorance",
+		color: "green"
+	},
+	resto : {
+		name: "Restoration Temptation",
+		color: "blue"
+	},
+	pot : {
+		name: "Pot Head",
+		color: "blue"
+	},
+	solo : {
+		name: "Solo Thief",
+		color: "blue"
+	},
+	eye : {
+		name: "Eye for an Eye"	,
+		color: "blue"
+	},
+	dread : {
+		name: "Dreadstump's Greed",
+		color: "blue"
+	},
+	rare : {
+		name: "Rare Obsession",
+		color: "blue"
+	}
 }
